c5b6267bc1554cc0447751f8a4b855ac888e7baa
sgost <jameslucky917@gmail.com>
2021-11-26 13:21:03 +0530

    changes done

--- src/components/Landing/Banner/banner.js
+++ src/components/Landing/Banner/banner.js
@@ -6,27 +6,15 @@ import bannerimg2 from "../../../images/landing/mob_banner.png"
 import animi2 from "../../../images/landing/bg.svg"
 import man2 from "../../../images/landing/man.svg"
 import Nav from "../../Nav"
+import Ripples from 'react-ripples'
 
 
 export default function Banner ()
 {
 
-    const parallax = ( e ) =>
-    {
-        document.querySelectorAll( '.img2' ).forEach( layer =>
-        {
-            const speed = layer.getAttribute( 'data-speed' )
-
-            const x = ( window.innerWidth - e.pageX * speed ) / 100
-            const y = ( window.innerHeight - e.pageY * speed ) / 100
-
-            layer.style.transform = `translateX(${x}px) translateY(${y}px)`
-        } )
-    }
-
     return (
 
-        <BannerSection onMouseMove={ parallax }>
+        <BannerSection>
             <Nav />
             <div id="bannerSectionContainer">
                 <div id="image_container">
@@ -37,12 +25,14 @@ export default function Banner ()
                     <div id="image_matter_b1">
                         <h1>Earn Peacefully</h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Justo, leo convallis adipiscing interdum mi. Scelerisque sit</p>
-                        <button>Know more</button>
+                        <Ripples color="#fff" during={2200}>
+                        <button type="button">Know more</button>
+                        </Ripples>
                     </div>
                     <div id="image_matter_b2">
-                        <img src={ animi2 } alt="img" id="img3" className="img2" data-speed="3" />
+                        <img src={ animi2 } alt="img" id="img3" className="img2" />
 
-                        <img src={ man2 } alt="img" id="img3" className="img2" data-speed="1.5" />
+                        <img src={ man2 } alt="img" id="img3" className="img2" />
                     </div>
                 </div>
             </div>
